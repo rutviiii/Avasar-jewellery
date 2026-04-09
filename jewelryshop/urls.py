75dbada9dc94b460265d20dc6bc3520d39553a1e
rutviiii <rutvipatela@gmail.com>
2026-04-09 15:37:12 +0530

    fix create-admin route order

--- jewelryshop/urls.py
+++ jewelryshop/urls.py
@@ -1,15 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from store.views import create_admin
 from django.conf import settings
 from django.conf.urls.static import static
-from store.views import create_admin
+
 
 urlpatterns = [
+    path('create-admin/', create_admin),
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('create-admin/', create_admin),
+    
 ]
 
 # To display images
