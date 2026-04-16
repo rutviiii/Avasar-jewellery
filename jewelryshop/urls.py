e2c083a534feb443f2ed648d99924c75cd33be6e
rutviiii <rutvipatela@gmail.com>
2026-04-16 19:59:27 +0530

    remove create_admin import error fix

--- jewelryshop/urls.py
+++ jewelryshop/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from store.views import create_admin
+
 from django.conf import settings
 from django.conf.urls.static import static
 
